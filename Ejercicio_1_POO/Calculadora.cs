d5673d04eb34414818f4f63cb8985434dd307d0f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-01-22 14:21:43 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio_1_POO/Calculadora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1_POO
+{
+    class Calculadora
+    {
+        //Declaramos variables/datos
+        public double Variable1;
+        public double Variable2;
+        public double Resultado;
+
+        public void Suma()
+        {
+            //Operacion Suma
+            Resultado = Variable1 + Variable2;
+
+        } 
+        public void Mostrar()
+        {
+            //Mostrar resultado
+            Console.WriteLine("Variable1={0}, Variable2={1}, resultado={2}", Variable1, Variable2, Resultado); 
+        }
+    }
+
+}
---
+++ Ejercicio_1_POO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            calculadora.Variable1 = 420;
+            calculadora.Variable2 = 911;
+            //Mostrar datos asignados a variables de constructor calculadora
+            calculadora.Mostrar();
+            //Se suman las variables
+            calculadora.Suma();
+            calculadora.Mostrar();
+            //Nuevo objeto misma clase
+            Calculadora calculadora2 = new Calculadora();
+            calculadora2.Variable1 = 69;
+            calculadora2.Variable2 = 54154;
+            calculadora2.Suma();
+            calculadora2.Mostrar();
+
+            Console.ReadLine();
+        }
+    }
+}
